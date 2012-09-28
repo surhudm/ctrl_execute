b0a54c22e1cfaaca284cb569d692e67caf1644ed
Stephen R. Pietrowicz <srp@ncsa.illinois.edu>
2012-09-28 13:28:12 -0500

    Added qdelete and qstatus commands

---
+++ bin/qstatus.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+# 
+# LSST Data Management System
+# Copyright 2008, 2009, 2010 LSST Corporation.
+# 
+# This product includes software developed by the
+# LSST Project (http://www.lsst.org/).
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the LSST License Statement and 
+# the GNU General Public License along with this program.  If not, 
+# see <http://www.lsstcorp.org/LegalNotices/>.
+#
+
+
+from __future__ import with_statement
+import sys, os, os.path, shutil, subprocess
+import lsst.pex.config as pexConfig
+from lsst.ctrl.execute.EnvString import EnvString
+from lsst.ctrl.execute.AllocationConfig import AllocationConfig
+from lsst.ctrl.execute.CondorInfoConfig import CondorInfoConfig
+import eups
+
+def runCommand(cmd):
+    cmd_split = cmd.split()
+    pid = os.fork()
+    if not pid:
+#        sys.stdin.close()
+#        sys.stdout.close()
+#        sys.stderr.close()
+#            os.close(0)
+#            os.close(1)
+#            os.close(2)
+        os.execvp(cmd_split[0], cmd_split)
+    os.wait()[0]
+
+if __name__ == "__main__":  
+    platform = sys.argv[1]
+    configFileName = "$HOME/.lsst/condor-info.py"
+    fileName = EnvString.resolve(configFileName)
+
+    condorInfoConfig = CondorInfoConfig()
+    condorInfoConfig.load(fileName)
+
+    platformPkgDir = eups.productDir("ctrl_platform_"+platform)
+    if platformPkgDir is not None:
+        configName = os.path.join(platformPkgDir, "etc", "config", "pbsConfig.py")
+    else:
+        raise RuntimeError("ctrl_platform_%s was not found.  Is it setup?" % platform)
+
+    allocationConfig = AllocationConfig()
+    allocationConfig.load(configName)
+
+    hostName = allocationConfig.platform.loginHostName
+    utilityPath = allocationConfig.platform.utilityPath
+    userName = condorInfoConfig.platform[platform].user.name
+    cmd = "gsissh %s %s/qstat -u%s" % (hostName, utilityPath, userName)
+    print cmd
+    runCommand(cmd)
